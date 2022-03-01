bbeb2a8eceef6eff46c6b09031bf3dacd818c0f0
Iman Khalid <imankhalid@Imans-MacBook-Air.local>
2022-03-01 15:26:33 +0400

    message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
 
-function App() {
+const ENDPOINT = "https://picsum.photos/v2/list";
+
+const fetchJSON = async (endpoint) =>
+  await fetch(endpoint).then((x) => x.json());
+
+export const useGetData = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const pullData = async () => {
+      const response = await fetchJSON(ENDPOINT);
+      console.log({ response });
+      setData(response);
+    };
+
+    pullData();
+  }, [setData]);
+
+  return data;
+};
+
+const App = () => {
+  const [amount, setAmount] = useState(0);
+  const less = () => setAmount(amount - 1);
+  const more = () => setAmount(amount + 1);
+
+  const [index, setIndex] = useState(null);
+  const data = useGetData();
+  if (!data) return <div>loading</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((x, i) => (
+        <button onClick={() => setIndex(i)}>{x.author}</button>
+      ))}
+      <br />
+      <br />
+      {index && (
+        <img src={data[index].download_url} al="" width="500" height="500" />
+      )}
+
+      <div data-test="main">
+        <p>How Many pieces do you want?</p>
+        <button onClick={less} data-test="lessbtn">
+          -
+        </button>
+        <pre id="currentAmount">{amount}</pre>
+        <button onClick={more} data-test="morebtn">
+          +
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
